91ea64d3d911eae406327cc1ef9e1c119f82c453
fletcher <fletcher@localhost.localdomain>
2020-03-15 09:30:58 -0700

    Updates

---
+++ precache-manifest.a34638ba0bfcfc33fc839159284ec50b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d556b15aea8f198c8075789f428b43d",
+    "url": "/coronavirus_app/index.html"
+  },
+  {
+    "revision": "f88a9f1a311734fc94cf",
+    "url": "/coronavirus_app/static/css/main.0fb03f44.chunk.css"
+  },
+  {
+    "revision": "c3ac64e3f169e3190f95",
+    "url": "/coronavirus_app/static/js/2.ae9c69c6.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/coronavirus_app/static/js/2.ae9c69c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88a9f1a311734fc94cf",
+    "url": "/coronavirus_app/static/js/main.696dc0c0.chunk.js"
+  },
+  {
+    "revision": "6180ba627ebad85de373",
+    "url": "/coronavirus_app/static/js/runtime-main.3ebb5514.js"
+  }
+]);
